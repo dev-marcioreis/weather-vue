c1486ab36d658ce4133d6920b0b3400a13afe6bb
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-04-29 11:07:00 -0300

    add img

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup>
-  
+<script>
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        api_key: 'd434f4c9ad064a905d8c5cd65770facf'
+      }
+    }
+  } 
+
 </script>
 
 <template>
-  <div id="app">
-    <h1>Em desenvolvimento...</h1>
-  </div>
+
+    <div id="app">
+      <main>
+        <div class="search-box">
+          <input type="text" class="search-bar" placeholder="Cidade..." />
+        </div>
+      </main>
+    </div>
+
 </template>
 
